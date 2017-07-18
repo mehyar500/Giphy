b230dd47be3542e5b17a1eabbf5263c240479293
mehyar500 <mehyar500@gmail.com>
2017-07-18 18:07:23 -0400

    newly pushed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	//display buttons
 	// Function that displays all buttons of the array
 	function renderButtons(){
-		// $("#animalButtons").empty();
+		$("#animalButtons").empty();
 		//loop through the array to render buttons
 		//check movie-button-layout-solved.html
 		for (var i =0; i < animal.length; i++){
@@ -23,18 +23,17 @@ $(document).ready(function(){
 	renderButtons();
 	console.log(animal);
 
-	// Function to add a new button
-	function addNewButton(){
-		$("#addAnimal").on("click", function(){
-			// event.preventDefault();
+		$("#animalForm").on("submit", function(event){
+			event.preventDefault();
 			var addAnimal = $("#animalInput").val().trim();
 			animal.push(addAnimal);
 			renderButtons();
 			console.log(animal);
+			return false;
 		});
-	};
-	console.log(animal);
-	// Function that displays all of the gifs
+
+	// console.log(animal);
+	// // Function that displays all of the gifs
 	function displayGifs(){
 		var animalDisplay = $(this).attr("data-name");
     	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animalDisplay + "&api_key=5c85886a63464a1bbdca4e06adc0e445&limit=10";
